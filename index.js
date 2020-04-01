4d6a31282f84c524f8131dd57dadfc12cef18702
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-04-01 16:44:06 +0200

    Use file class to hash file

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const fs = require("fs");
-const path = require("path");
 const findAssetFiles = require("./lib/findAssetFiles.js");
-const md5Hash = require("./lib/md5Hash.js");
 
 function createMixManifest(assetsDir, fileToBeCreated) {
   const files = findAssetFiles(assetsDir);
   let manifest = {};
 
   for (let file of files) {
-    manifest[file.assetPath] =
-      file.assetPath + "?id=" + md5Hash(fs.readFileSync(file.path));
+    manifest[file.assetPath] = file.assetPath + "?id=" + file.hash;
   }
 
   fs.writeFileSync(fileToBeCreated, JSON.stringify(manifest));
--- tests/hashableFile.test.js
+++ tests/hashableFile.test.js
@@ -1,5 +1,7 @@
+const helper = require("./helpers/helper.js");
 const HashableFile = require("../lib/hashableFile.js");
 const test = require("ava");
+const path = require("path");
 
 test("#assetPath returns the given path relative to an assets dir", (t) => {
   const file = new HashableFile("", "/assets/foo.png");
@@ -11,4 +13,20 @@ test("#path returns the given full path to the file", (t) => {
   const file = new HashableFile("/full/path/assets/foo.png", "");
 
   t.is("/full/path/assets/foo.png", file.path);
+});
+
+test("#hash returns the hash of the file contents", (t) => {
+  let lastContent = "";
+
+  const file = new HashableFile(
+    path.join(helper.FIXTURES_DIR, "hasher", "one.txt"),
+    "",
+    (content) => {
+      lastContent = content;
+      return "stubHash";
+    }
+  );
+
+  t.is("stubHash", file.hash);
+  t.is("a\n", lastContent);
 });

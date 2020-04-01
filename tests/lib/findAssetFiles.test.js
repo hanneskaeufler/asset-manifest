5726336513144ff078c5505505679cec2f68a78a
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-04-01 15:41:55 +0200

    Add finder function to scan directory

--- tests/lib/findAssetFiles.test.js
+++ tests/lib/findAssetFiles.test.js
@@ -7,7 +7,19 @@ test("it finds toplevel js/png/svg/... files", (t) => {
   const files = findAssetFiles(path.join(helper.FIXTURES_DIR, "finder"));
 
   t.deepEqual(
-    ["image.png", "logo.svg", "stylesheet.css", "script.js"].sort(),
+    ["/image.png", "/logo.svg", "/stylesheet.css", "/script.js"].sort(),
     files
   );
+});
+
+test("it ignores folders", (t) => {
+  const files = findAssetFiles(path.join(helper.FIXTURES_DIR, "folders"));
+
+  t.deepEqual([], files);
+});
+
+test("it finds file in nested folder", (t) => {
+  const files = findAssetFiles(path.join(helper.FIXTURES_DIR, "nested"));
+
+  t.deepEqual(["/logos/corp.svg"], files);
 });

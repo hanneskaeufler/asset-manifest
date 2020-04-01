065b56c8f449462109441e2b149e340282240da8
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2020-04-01 15:01:52 +0200

    Create a manifest json file

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+function createMixManifest(assetsDir, fileToBeCreated) {
+    fs.writeFileSync(fileToBeCreated, '{"/assets/main.css": "..."}');
+}
+
+// 0    1        2         3
+// node index.js assetsDir fileToBeCreated
+createMixManifest(process.argv[2], process.argv[3]);
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,41 @@
+const prcs = require("child_process");
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
 const test = require("ava");
 
-test("Something", t => {
-    t.fail();
+const ASSETS_FILE_NAME = "mix-manifest.json"
+const FIXTURES_DIR = path.join("./tests", "fixtures");
+const FIXTURE_ASSETS = path.join(FIXTURES_DIR, "assets");
+
+const inTempDir = (fn) => {
+    const folder = fs.mkdtempSync(path.join(os.tmpdir(), "asset-manifest-test-"));
+    fn(folder);
+};
+
+const createMixManifest = (assetsDir, manifestFile) => {
+    prcs.execFileSync("node", ["index.js", assetsDir, manifestFile]);
+}
+
+test("it creates the given file", t => {
+    inTempDir((folder) => {
+        const target = path.join(folder, ASSETS_FILE_NAME);
+
+        createMixManifest(FIXTURE_ASSETS, target);
+
+        t.truthy(fs.existsSync(target));
+    });
+});
+
+test("the created file has an entry for each asset file in the given assets folder", t => {
+    inTempDir((folder) => {
+        const target = path.join(folder, ASSETS_FILE_NAME);
+
+        createMixManifest(FIXTURE_ASSETS, target);
+
+        const fileContent = fs.readFileSync(target);
+        t.deepEqual({
+            "/assets/main.css": "..."
+        }, JSON.parse(fileContent));
+    });
 });
